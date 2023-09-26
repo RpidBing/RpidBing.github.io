bf1aa4ffc3faf2599ef2a6a991c152b07d137d40
Rpid <2059162753@qq.com>
2023-09-26 09:39:11 +0800

    revise astro.config.mjs site parameter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'RpidBing.github.io'
+    site: 'https://RpidBing.github.io'
 });
    
